a12490b59e0da44317214d84586c21a95efb7c66
Hector Flores <hecto932@gmail.com>
2017-04-19 00:45:27 -0500

    Definiendo la clase punto

--- 6-distancia.js
+++ 6-distancia.js
@@ -12,7 +12,7 @@ Punto.prototype.distancia = function distancia(p) {
   return Math.sqrt(x * x + y * y)
 }*/
 
-const Punto = {
+/*const Punto = {
   init: function init(x, y) {
     this.x = x
     this.y = y
@@ -29,12 +29,32 @@ const Punto = {
 
     return Math.sqrt(x * x + y * y)
   }
+}*/
+
+class Punto {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  moverEnX(x) {
+    this.x += x
+  }
+
+  moverEnY(y) {
+    this.y += y
+  }
+
+  distancia(p) {
+    const x = p1.x - p2.x 
+    const y = p1.y - p2.y
+
+    return Math.sqrt(x * x + y * y)
+  }
 }
 
-const p1 = Object.create(Punto)
-const p2 = Object.create(Punto)
-p1.init(0, 4)
-p2.init(3, 0)
+const p1 = new Punto(0, 4)
+const p2 = new Punto(3, 0)
 
 console.log(p1.distancia(p2))
 console.log(p2.distancia(p1))
