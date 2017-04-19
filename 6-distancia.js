e6d31f5f270ff9cc63f662e9cd5d63a2841ddc85
Hector Flores <hecto932@gmail.com>
2017-04-19 00:34:53 -0500

    Creando objetos usando prototipos

--- 6-distancia.js
+++ 6-distancia.js
@@ -1,26 +1,23 @@
-const p1 = {
-  x: 0,
-  y: 4,
-  moverEnX(x) { this.x += x },
-  moverEnY(y) { this.y += y }
+function Punto(x, y) {
+  this.x = x
+  this.y = y
 }
 
-const p2 = {
-  x: 3,
-  y: 0,
-  moverEnX(x) { this.x += x },
-  moverEnY(y) { this.y += y }
-}
-
-function distancia (p1, p2) {
+Punto.prototype.moverEnX = function moverEnX(x) { this.x += x }
+Punto.prototype.moverEnY = function moverEnY(y) { this.y += y }
+Punto.prototype.distancia = function distancia(p) {
   const x = p1.x - p2.x 
   const y = p1.y - p2.y
 
-  return Math.sqrt((x * x + y * y))
+  return Math.sqrt(x * x + y * y)
 }
 
-console.log(distancia(p1, p2))
+const p1 = new Punto(0, 4)
+const p2 = new Punto(3, 0)
 
+console.log(p1.distancia(p2))
+console.log(p2.distancia(p1))
 p1.moverEnX(10)
-
-console.log(distancia(p1, { x: 20, y: -7}))
+console.log(p1.distancia(p2))
+p2.moverEnY(-4)
+console.log(p1.distancia(p2))
